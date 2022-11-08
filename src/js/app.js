9203164288ab916d064f0377c0349e0b5e505006
snezhka003 <you@example.com>
2022-11-08 11:03:38 +0300

    classes

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,19 @@ import Magician from './magician';
 import Swordsman from './swordsman';
 import Bowerman from './bowerman';
 
-const zombie = new Zombie('Zombie');
-console.log(zombie);
-const undead = new Undead('Undead');
-console.log(undead);
-const daemon = new Daemon('Daemon');
-console.log(daemon);
-const magician = new Magician('Magician');
-console.log(magician);
-const swordsman = new Swordsman('Swordsman');
-console.log(swordsman);
-const bowerman = new Bowerman('Bowerman');
-console.log(bowerman);
+class Game {
+  constructor() {
+    this.activeCharacters = [];
+    this.activeCharacters.push(
+      new Zombie('zombie'),
+      new Undead('undead'),
+      new Daemon('daemon'),
+      new Magician('magician'),
+      new Swordsman('swordsman'),
+      new Bowerman('bowerman'),
+    );
+  }
+}
+
+const game = new Game();
+console.log(game);
